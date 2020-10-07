2f1d28219c3cf91e9f619f292cf0036070e68410
mcgicjn2 <jonathan.nuttall@deltares.nl>
2020-10-07 12:54:01 +0200

    Added necessary files and adapted the read code from OURS\CPTtool
    Also added some basic tests
    
    Issue : GEOLIBPLUS-7

--- geolib_plus/__init__.py
+++ geolib_plus/__init__.py
@@ -2,9 +2,7 @@ __version__ = "0.1.0"
 
 from abc import abstractmethod
 from pathlib import Path
-
 from pydantic import BaseModel
-
 
 class AbstractInterpretationMethod:
     """Base Interpretation method for analyzing CPTs."""
@@ -15,30 +13,78 @@ class RobertsonMethod(AbstractInterpretationMethod):
 
 
 class AbstractCPT(BaseModel):
+
     """Base CPT class, should define abstract."""
+    
+    #  variables
+    depth = []
+    coordinates = []
+    local_reference_level = []
+    depth_to_reference = []
+    tip = []
+    friction = []
+    friction_nbr = []
+    a = []
+    name = []
+    gamma = []
+    rho = []
+    total_stress = []
+    effective_stress = []
+    pwp = []
+    qt = []
+    Qtn = []
+    Fr = []
+    IC = []
+    n = []
+    vs = []
+    G0 = []
+    E0 = []
+    permeability = []
+    poisson = []
+    damping = []
+
+    water = []
+    lithology = []
+    litho_points = []
+
+    lithology_merged = []
+    depth_merged = []
+    index_merged = []
+    vertical_datum = []
+    local_reference = []
+    inclination_resultant = []
+    cpt_standard = []
+    quality_class = []
+    cpt_type = []
+    result_time = []
+
+    # NEN results
+    litho_NEN = []
+    E_NEN = []
+    cohesion_NEN = []
+    fr_angle_NEN = []
+
+    # fixed values
+    g = 9.81
+    Pa = 100.
+
+    # private variables
+    __water_measurement_types = None
 
     @classmethod
     @abstractmethod
     def read(cls, file: Path):
         pass
 
-    @property
-    @abstractmethod
-    def valid(self) -> bool:
-        pass
-
-    @abstractmethod
-    def interpret(self, method: AbstractInterpretationMethod) -> "Profile":
-        pass
-
-    @abstractmethod
-    def plot(self):
-        pass
-
-
-class GEF_CPT(AbstractCPT):
-    pass
-
-
-class XML_CPT(AbstractCPT):
-    pass
+    # @property
+    # @abstractmethod
+    # def valid(self) -> bool:
+    #     pass
+    #
+    # @abstractmethod
+    # def interpret(self, method: AbstractInterpretationMethod) -> "Profile":
+    #     pass
+    #
+    # @abstractmethod
+    # def plot(self):
+    #     pass
