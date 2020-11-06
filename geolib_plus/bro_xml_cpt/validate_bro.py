1ef57724f5859481ae6806bc8239c819e3209d57
EleniSmyrniou <eleni.smyrniou@deltares.nl>
2020-11-06 08:55:32 +0100

    Moved validate functions

--- geolib_plus/bro_xml_cpt/validate_bro.py
+++ geolib_plus/bro_xml_cpt/validate_bro.py
@@ -1,38 +1,45 @@
 from lxml import etree
 from urllib.parse import urlparse
 import requests
+import logging
+import numpy as np
+
 
 class HTTPSResolver(etree.Resolver):
-    __name__ = 'HTTPSResolver'
+    __name__ = "HTTPSResolver"
 
     def resolve(self, url, id_, context):
         url_components = urlparse(url)
         scheme = url_components.scheme
 
         # the resolver will only check for redirects if http/s is present
-        if scheme == 'http' or scheme == 'https':
+        if scheme == "http" or scheme == "https":
             head_response = requests.head(url, allow_redirects=True)
             new_request_url = head_response.url
             if len(head_response.history) != 0:
                 # recursively, resolve the ultimate redirection target
                 return self.resolve(new_request_url, id, context)
             else:
-                if scheme == 'http':
+                if scheme == "http":
                     # libxml2 can handle this resource
                     return self.resolve_filename(new_request_url, context)
-                elif scheme == 'https':
+                elif scheme == "https":
                     # libxml2 cannot resolve this resource, so do the work
                     get_response = requests.get(new_request_url)
-                    return self.resolve_string(get_response.content, context,
-                                                base_url=new_request_url)
+                    return self.resolve_string(
+                        get_response.content, context, base_url=new_request_url
+                    )
                 else:
                     raise ValueError(f"{__name__}, Scheme should be http or https")
         else:
             # treat resource as a plain old file
             return self.resolve_filename(url, context)
 
+
 def validate_bro(schema_url, bro_xml_file):
-    bro_tree = etree.parse(bro_xml_file).find('{http://www.broservices.nl/xsd/dscpt/1.1}dispatchDocument')[0]
+    bro_tree = etree.parse(bro_xml_file).find(
+        "{http://www.broservices.nl/xsd/dscpt/1.1}dispatchDocument"
+    )[0]
     parser = etree.XMLParser(load_dtd=True)
     resolver = HTTPSResolver(schema_url)
     parser.resolvers.add(resolver)
@@ -43,12 +50,58 @@ def validate_bro(schema_url, bro_xml_file):
     else:
         xml_validator.assertValid(bro_tree)
 
+
 def validate_bro_cpt(bro_xml_file):
-    schema_url = r'https://schema.broservices.nl/xsd/dscpt/1.1/dscpt-messages.xsd'
+    schema_url = r"https://schema.broservices.nl/xsd/dscpt/1.1/dscpt-messages.xsd"
     return validate_bro(schema_url, str(bro_xml_file))
 
 
+class ValidatePreProcessing:
+    def __check_file_contains_data(self, cpt):
+        if len(cpt.penetration_length) == 0:
+            logging.warning("File " + cpt.name + " contains no data")
+            return
 
+    def __check_data_different_than_zero(self, cpt):
+        keys = [
+            "penetration_length",
+            "tip",
+            "friction",
+            "friction_ratio",
+        ]
+        for k in keys:
+            if all(getattr(cpt, k) == 0) or all(getattr(cpt, k) == None):
+                logging.warning("File " + cpt.name + " contains empty data")
+                return
 
+    def __check_criteria_minimum_length(self, cpt, minimum_length: int):
+        if np.max(np.abs(cpt.penetration_length)) < minimum_length:
+            logging.warning(
+                "File " + cpt.name + " has a length smaller than " + str(minimum_length)
+            )
+            return
 
+    def __check_minimum_sample_criteria(self, cpt, minimum_samples: int):
+        if len(cpt.penetration_length) < minimum_samples:
+            logging.warning(
+                "File "
+                + cpt.name
+                + " has a number of samples smaller than "
+                + str(minimum_samples)
+            )
+            return
 
+    def validate_length_and_samples_cpt(
+        self, cpt, minimum_length: int = 5, minimum_samples: int = 50
+    ):
+        """
+        Performs initial checks regarding the availability of
+        data in the cpt. Returns a string that contains all the
+        error messages.
+        """
+
+        self.__check_file_contains_data(cpt)
+        self.__check_data_different_than_zero(cpt)
+        self.__check_criteria_minimum_length(cpt, minimum_length)
+        self.__check_minimum_sample_criteria(cpt, minimum_samples)
+        return
