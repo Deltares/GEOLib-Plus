7ad16b6a3070dc2c2f124aab33a51d120a014de4
noordam <aron.noordam@deltares.nl>
2021-09-20 10:18:37 +0200

    added tests for state prob parameters

--- geolib_plus/shm/state_utils.py
+++ geolib_plus/shm/state_utils.py
@@ -25,12 +25,10 @@ class StateUtils(BaseModel):
         return effective_stress * (su / (effective_stress * S)) ** 1 / m
 
     @staticmethod
-    def calculate_yield_stress_prob_parameters_from_cpt(effective_stress: float, su: np.ndarray, q_net: np.ndarray,
-                                                        mu_S: float, mu_m:float,
-                                                        method: NktMethod = NktMethod.STATISTICS) -> (float, float):
+    def calculate_yield_stress_prob_parameters_from_cpt(effective_stress: float, q_net: float, mu_S: float, mu_m: float,
+                                                        mean_nkt: float, vc_q_net_nkt: float) -> (float, float):
         """
-        Calculates probabilistic parameters of the yield stress, using cpt data and lab data. Firstly the mean and VC of
-        qnet/Nkt is calculated through regression or statistics, using su and q_net data. Through the shansep relation
+        Calculates probabilistic parameters of the yield stress, using Nkt and q_net data. Through the shansep relation
         the qnet_Nkt is used to calculate the yield stress.  The standard deviation of the yield stress is calculated
         by:
 
@@ -39,38 +37,32 @@ class StateUtils(BaseModel):
             \sigma_{vy}' = \frac{\frac{q_{net}}/{N_{kt}}}{\mu_{S}} * Vc_{ \frac{q_{net}}/{N_{kt}}}
 
         :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest
-        :param su: Undrained Shear Strength [kPa] test set
-        :param q_net: Cone resistance corrected with water pressure [kPa] test set
+        :param q_net: Cone resistance corrected with water pressure [kPa] at the depth of interest
         :param mu_S: Mean normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
         :param mu_m: Mean Strength Increase Exponent [-] (input from laboratory)
-        :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param mean_nkt: Mean of nkt value
+        :param vc_q_net_nkt: Variation coefficient of qnet/N_kt
 
         :return: mean of yield stress, std of yield stress
         """
 
-        # calculates mean and variation coefficient of qnet/Nkt
-        if method == NktMethod.STATISTICS:
-            mean_qnet_nkt, vc_qnet_nkt = NktUtils.get_prob_nkt_parameters_from_statistics(su, q_net)
-        elif method == NktMethod.REGRESSION:
-            mean_qnet_nkt, vc_qnet_nkt = NktUtils.get_prob_nkt_parameters_from_weighted_regression(su, q_net)
-        else:
-            return None, None
+        # calculates mean of qnet/Nkt
+        mean_qnet_nkt = q_net/mean_nkt
 
         # calculate yield stress through shansep relation
         mu_yield = StateUtils.calculate_yield_stress(mean_qnet_nkt, effective_stress, mu_S, mu_m)
 
         # calculate standard deviation of yield stress
-        sigma_yield = mean_qnet_nkt/mu_S * vc_qnet_nkt
+        sigma_yield = mean_qnet_nkt/mu_S * vc_q_net_nkt
 
         return mu_yield, sigma_yield
 
     @staticmethod
-    def calculate_pop_prob_parameters_from_cpt(effective_stress: float, su: np.ndarray, q_net: np.ndarray, mu_S: float,
-                                               mu_m: float, method: NktMethod = NktMethod.STATISTICS) -> (float, float):
+    def calculate_pop_prob_parameters_from_cpt(effective_stress: float, q_net: float, mu_S: float, mu_m: float,
+                                                        mean_nkt: float, vc_q_net_nkt: float) -> (float, float):
         """
         Calculates probabilistic parameters of the pre overburden pressure, using cpt data and lab data.
         Firstly the mean and standard deviation of the yield stress are calculated. The pop is then calculated by:
-
 
         .. math::
 
@@ -78,17 +70,18 @@ class StateUtils(BaseModel):
             \sigma_{pop} = \sigma_{\sigma_{vy}'}
 
         :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest
-        :param su: Undrained Shear Strength [kPa] test set
-        :param q_net: Cone resistance corrected with water pressure [kPa] test set
+        :param q_net: Cone resistance corrected with water pressure [kPa] at the depth of interest
         :param mu_S: Mean normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
         :param mu_m: Mean Strength Increase Exponent [-] (input from laboratory)
-        :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param mean_nkt: Mean of nkt value
+        :param vc_q_net_nkt: Variation coefficient of qnet/N_kt
 
         :return: mean of pop, std of pop
         """
 
-        mu_yield, sigma_yield = StateUtils.calculate_yield_stress_prob_parameters_from_cpt(effective_stress, su, q_net,
-                                                                                           mu_S, mu_m, method=method)
+        mu_yield, sigma_yield = StateUtils.calculate_yield_stress_prob_parameters_from_cpt(effective_stress, q_net,
+                                                                                           mu_S, mu_m, mean_nkt,
+                                                                                           vc_q_net_nkt)
 
         mu_pop = mu_yield - effective_stress
         sigma_pop = sigma_yield
@@ -96,12 +89,11 @@ class StateUtils(BaseModel):
         return mu_pop, sigma_pop
 
     @staticmethod
-    def calculate_ocr_prob_parameters_from_cpt(effective_stress, su, q_net, mu_S, m,
-                                               method: NktMethod = NktMethod.STATISTICS):
+    def calculate_ocr_prob_parameters_from_cpt(effective_stress: float, q_net: float, mu_S: float, mu_m: float,
+                                                        mean_nkt: float, vc_q_net_nkt: float) -> (float, float):
         """
         Calculates probabilistic parameters of the over consolidation ration, using cpt data and lab data.
         Firstly the mean and standard deviation of the yield stress are calculated. The ocr is then calculated by:
-
 
         .. math::
 
@@ -109,20 +101,20 @@ class StateUtils(BaseModel):
             \sigma_{ocr} = \frac{\sigma_{\sigma_{vy}'}}{\mu_{\sigma_{vy}'}} * \mu_{ocr}
 
         :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest
-        :param su: Undrained Shear Strength [kPa] test set
-        :param q_net: Cone resistance corrected with water pressure [kPa] test set
+        :param q_net: Cone resistance corrected with water pressure [kPa] at the depth of interest
         :param mu_S: Mean normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
         :param mu_m: Mean Strength Increase Exponent [-] (input from laboratory)
-        :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param mean_nkt: Mean of nkt value
+        :param vc_q_net_nkt: Variation coefficient of qnet/N_kt
 
         :return: mean of ocr, std of ocr
         """
 
-        mu_yield, sigma_yield = StateUtils.calculate_yield_stress_prob_parameters_from_cpt(effective_stress, su, q_net,
-                                                                                           mu_S, m,
-                                                                                           method=method)
+        mu_yield, sigma_yield = StateUtils.calculate_yield_stress_prob_parameters_from_cpt(effective_stress, q_net,
+                                                                                           mu_S, mu_m, mean_nkt,
+                                                                                           vc_q_net_nkt)
 
-        mu_ocr = mu_yield /np.mean(effective_stress)
+        mu_ocr = mu_yield / np.mean(effective_stress)
 
         # Vc yield = Vc Ocr
         sigma_ocr = sigma_yield/mu_yield * mu_ocr
@@ -130,29 +122,22 @@ class StateUtils(BaseModel):
         return mu_ocr, sigma_ocr
 
     @staticmethod
-    def calculate_characteristic_yield_stress(effective_stress: float, current_effective_stress: float, su: np.ndarray,
-                                              q_net: np.ndarray, kar_S: float, kar_m: float,
-                                              method: NktMethod = NktMethod.STATISTICS) -> float:
+    def calculate_characteristic_yield_stress(effective_stress: float, current_effective_stress: float,
+                                              q_net: float, kar_S: float, kar_m: float, char_nkt) -> float:
         """
         Calculates characteristic value of yield stress, using cpt data and lab data.
 
         :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest during cpt
-        :param effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
-        :param su: Undrained Shear Strength [kPa] test set
-        :param q_net: Cone resistance corrected with water pressure [kPa] test set
+        :param current_effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
+        :param q_net: Cone resistance corrected with water pressure [kPa]
         :param kar_S: Characteristic value normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
         :param kar_m: Characteristic Strength Increase Exponent [-] (input from laboratory)
-        :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param char_nkt: characteristic value of nkt
 
         :return: characteristic value of yield stress
         """
 
-        if method == NktMethod.STATISTICS:
-            char_qnet_nkt = NktUtils.get_characteristic_value_nkt_from_statistics(su, q_net)/np.mean(q_net)
-        elif method == NktMethod.REGRESSION:
-            char_qnet_nkt = NktUtils.get_characteristic_value_nkt_from_weighted_regression(su, q_net)/np.mean(q_net)
-        else:
-            return None
+        char_qnet_nkt = q_net/char_nkt
 
         yield_char = StateUtils.calculate_yield_stress(char_qnet_nkt, effective_stress, kar_S, kar_m)
         yield_char = max(yield_char, max(effective_stress,current_effective_stress))
@@ -160,49 +145,45 @@ class StateUtils(BaseModel):
         return yield_char
 
     @staticmethod
-    def calculate_characteristic_pop(effective_stress: float, current_effective_stress: float, su:np.ndarray,
-                                     q_net: np.ndarray, kar_S: float, kar_m: float,
-                                     method: NktMethod = NktMethod.STATISTICS) -> float:
+    def calculate_characteristic_pop(effective_stress: float, current_effective_stress: float,
+                                              q_net: float, kar_S: float, kar_m: float, char_nkt) -> float:
         """
         Calculates characteristic value of pre overburden pressure, using cpt data and lab data.
 
         :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest during cpt
-        :param effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
-        :param su: Undrained Shear Strength [kPa] test set
-        :param q_net: Cone resistance corrected with water pressure [kPa] test set
+        :param current_effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
+        :param q_net: Cone resistance corrected with water pressure [kPa]
         :param kar_S: Characteristic value normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
         :param kar_m: Characteristic Strength Increase Exponent [-] (input from laboratory)
-        :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param char_nkt: characteristic value of nkt
 
         :return: characteristic value of pre overburden pressure
         """
 
         yield_char = StateUtils.calculate_characteristic_yield_stress(effective_stress, current_effective_stress,
-                                                                      su, q_net, kar_S, kar_m, method=method)
+                                                                      q_net, kar_S, kar_m, char_nkt)
 
         pop_char = max(yield_char - max(effective_stress, current_effective_stress), 0)
         return pop_char
 
     @staticmethod
-    def calculate_characteristic_ocr(effective_stress: float, current_effective_stress: float, su:np.ndarray,
-                                     q_net: np.ndarray, kar_S: float, kar_m: float,
-                                     method: NktMethod = NktMethod.STATISTICS) -> float:
+    def calculate_characteristic_ocr(effective_stress: float, current_effective_stress: float,
+                                              q_net: float, kar_S: float, kar_m: float, char_nkt) -> float:
         """
        Calculates characteristic value of over consolidation ratio, using cpt data and lab data.
 
-       :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest during cpt
-       :param effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
-       :param su: Undrained Shear Strength [kPa] test set
-       :param q_net: Cone resistance corrected with water pressure [kPa] test set
-       :param kar_S: Characteristic value normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
-       :param kar_m: Characteristic Strength Increase Exponent [-] (input from laboratory)
-       :param method: methodology of calculation yield stress (NktMethod.STATISTICS or NktMethod.REGRESSION)
+        :param effective_stress: Vertical In situ Effective Stress [kPa] at the depth of interest during cpt
+        :param current_effective_stress: Vertical current Effective Stress [kPa] at the depth of interest
+        :param q_net: Cone resistance corrected with water pressure [kPa]
+        :param kar_S: Characteristic value normally Consolidated Undrained Shear Strength Ratio [-] (input from laboratory)
+        :param kar_m: Characteristic Strength Increase Exponent [-] (input from laboratory)
+        :param char_nkt: characteristic value of nkt
 
        :return: characteristic value of over consolidation ratio
        """
 
         yield_char = StateUtils.calculate_characteristic_yield_stress(effective_stress, current_effective_stress,
-                                                                      su, q_net, kar_S, kar_m, method=method)
+                                                                      q_net, kar_S, kar_m, char_nkt)
 
         ocr_char = max(yield_char/ max(effective_stress, current_effective_stress), 0)
 
--- tests/shm/test_state_utils.py
+++ tests/shm/test_state_utils.py
@@ -1,4 +1,5 @@
 from geolib_plus.shm.state_utils import StateUtils
+from geolib_plus.shm.nkt_utils import NktMethod
 
 from pathlib import Path
 import numpy as np
@@ -26,18 +27,64 @@ class TestNktUtils:
         # assert
         assert pytest.approx(expected_yield_stress,1e-4) == calculated_yield_stress
 
-        # # check nkt mean and std for saturated soil
-        # is_saturated = True
-        # nkt_mean, nkt_std = NktUtils.get_default_nkt(is_saturated)
-        # expected_nkt_mean, expected_nkt_std = 20, 5
-        #
-        # assert pytest.approx(expected_nkt_mean) == nkt_mean
-        # assert pytest.approx(expected_nkt_std) == nkt_std
-        #
-        # # check nkt mean and std for unsaturated soil
-        # is_saturated = False
-        # nkt_mean, nkt_std = NktUtils.get_default_nkt(is_saturated)
-        # expected_nkt_mean, expected_nkt_std = 60, 15
-        #
-        # assert pytest.approx(expected_nkt_mean) == nkt_mean
-        # assert pytest.approx(expected_nkt_std) == nkt_std
+    @pytest.mark.unittest
+    def test_calculate_yield_stress_prob_parameters_from_cpt(self):
+        """
+        Tests  calculating mean and standard deviation of yield stress
+        """
+
+        mean_yield_stress, std_yield_stress = StateUtils.calculate_yield_stress_prob_parameters_from_cpt(15.8, 180,
+                                                                                                         0.38, 0.8,
+                                                                                                         16.01, 0.169)
+        expected_mean = 36.983464282192045
+        expected_std = 5.0001643709523655
+
+        assert pytest.approx(expected_mean) == mean_yield_stress
+        assert pytest.approx(expected_std) == std_yield_stress
+
+
+    @pytest.mark.unittest
+    def test_calculate_pop_prob_parameters_from_cpt(self):
+        """
+        Tests  calculating mean and standard deviation of pre overburden pressure
+        """
+        mean_pop, std_pop = StateUtils.calculate_pop_prob_parameters_from_cpt(15.8, 180, 0.38, 0.8, 16.01, 0.169)
+
+        expected_mean = 21.183464282192045
+        expected_std = 5.0001643709523655
+
+        assert pytest.approx(expected_mean) == mean_pop
+        assert pytest.approx(expected_std) == std_pop
+
+
+    @pytest.mark.unittest
+    def test_calculate_ocr_prob_parameters_from_cpt(self):
+        """
+        Tests  calculating mean and standard deviation of over consolidation ratio
+        """
+        mean_ocr, std_ocr = StateUtils.calculate_ocr_prob_parameters_from_cpt(16.4, 180, 0.38, 0.8, 16.01, 0.169)
+
+        expected_mean = 2.255089285499515
+        expected_std = 0.30488807139953444
+
+        assert pytest.approx(expected_mean) == mean_ocr
+        assert pytest.approx(expected_std) == std_ocr
+
+    @pytest.fixture
+    def su_qnet_data(self):
+        """
+        reads test data
+        """
+        import csv
+
+        # read test data
+        with open(TestUtils.get_local_test_data_dir(Path("shm", "su_qnet.csv"))) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            data = np.array([row for row in csv_reader]).astype(float)
+
+        # transform q_net and su to kPa
+        q_net = data[:, 0] * 1000
+        su = data[:, 1] * 1000
+        return su, q_net
+
+
