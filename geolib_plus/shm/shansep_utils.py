8c75e6761e38ca4d5cb97f2abf4ce9129128113a
noordam <aron.noordam@deltares.nl>
2021-09-23 09:52:45 +0200

    corrected calculate shansep prob parameters with linear regression

--- geolib_plus/shm/shansep_utils.py
+++ geolib_plus/shm/shansep_utils.py
@@ -1,8 +1,9 @@
-from math import log
 from pydantic import BaseModel
 from typing import Union, Optional
 import numpy as np
-from scipy.optimize import curve_fit, minimize
+from scipy.optimize import curve_fit
+
+from geolib_plus.shm.prob_utils import ProbUtils
 
 
 class ShansepUtils(BaseModel):
@@ -32,16 +33,15 @@ class ShansepUtils(BaseModel):
         return m * x + B
 
     @staticmethod
-    def get_shansep_parameters(
+    def get_shansep_prob_parameters_with_linear_regression(
         OCR: Union[float, np.array],
         su: Union[float, np.array],
         sigma_effective: Union[float, np.array],
         S: Optional[float] = None,
         m: Optional[float] = None,
-    ):
+    ) -> ((float, float), (float, float)):
         """
-        Determines shansep parameters s and m according to :cite: `meer_2019`.
-        This is done by using simple linear regression.
+        Determines shansep parameters s and m with linear regression according to :cite: `meer_2019`.
         Parameter S or m can also be given as an input.
 
         :param OCR: Union[float, np.array],
@@ -49,22 +49,31 @@ class ShansepUtils(BaseModel):
         :param sigma_effective: Effective stress
         :param S: S value
         :param m: m value
+        :return: (mean and std of S), (mean and std of m)
         """
         if (m is None) and (S is None):
             log_OCR = np.log(OCR)
             log_su_sigma = np.log(np.divide(su, sigma_effective))
 
-            popt, _ = curve_fit(
+            popt, cov = curve_fit(
                 ShansepUtils.__S_and_m_not_inputted, log_OCR, log_su_sigma, method="lm"
             )
+
             # summarize the parameter values
             m, log_S = popt
-            S = np.exp(log_S)
+            std_m, std_log_s = np.sqrt(cov[0,0]), np.sqrt(cov[1, 1])
+
+            # correct std with student t distribution
+            std_m = ProbUtils.correct_std_with_student_t(len(log_su_sigma)-1, 0.05,std_m, 0.75)
+            std_log_s = ProbUtils.correct_std_with_student_t(len(log_su_sigma) - 1, 0.05, std_log_s, 0.75)
+
+            S, std_s = ProbUtils.get_mean_std_from_lognormal(log_S, std_log_s)
+
         elif (m is None) and (S is not None):
             log_OCR = np.log(OCR)
             log_su_sigma = np.log(np.divide(su, sigma_effective))
 
-            popt, _ = curve_fit(
+            popt, cov = curve_fit(
                 ShansepUtils.__S_inputted,
                 (log_OCR, np.full(log_OCR.shape, np.log(S))),
                 log_su_sigma,
@@ -72,17 +81,34 @@ class ShansepUtils(BaseModel):
             )
             # summarize the parameter values
             m = popt[0]
+            std_m = np.sqrt(cov[0,0])
+
+            # correct std with student t distribution
+            std_m = ProbUtils.correct_std_with_student_t(len(log_su_sigma) - 1, 0.05, std_m, 0.75)
+            std_s = 0
+
         elif (m is not None) and (S is None):
             log_OCR = np.log(OCR)
             log_su_sigma = np.log(np.divide(su, sigma_effective))
 
-            popt, _ = curve_fit(
+            popt, cov = curve_fit(
                 ShansepUtils.__m_inputted,
                 (log_OCR, np.full(log_OCR.shape, m)),
                 log_su_sigma,
                 method="lm",
             )
             # summarize the parameter values
-            S = np.exp(popt[0])
+            log_S = popt[0]
+            std_log_s = np.sqrt(cov[0, 0])
 
-        return (S, m)
+            # correct std with student t distribution
+            std_log_s = ProbUtils.correct_std_with_student_t(len(log_su_sigma) - 1, 0.05, std_log_s, 0.75)
+            S, std_s = ProbUtils.get_mean_std_from_lognormal(log_S, std_log_s)
+            std_m = 0
+
+        else:
+            # both s and m are given
+            std_s = 0
+            std_m = 0
+
+        return (S, std_s), (m, std_m)
--- tests/shm/test_shansep_utils.py
+++ tests/shm/test_shansep_utils.py
@@ -9,45 +9,52 @@ import numpy as np
 
 class TestShanshepUtils:
     @pytest.mark.unittest
-    def test_get_shansep_parameters(self):
+    def test_get_shansep_prob_parameters_with_linear_regression(self):
         # denife inputs
         path_inputs = TestUtils.get_local_test_data_dir(
             Path("shm", "Data_KIJK_DSS.csv")
         )
         inputs = pd.read_csv(path_inputs, delimiter=";")
-        mask = inputs.TestConditions == "In situ"
-        inputs_modified = inputs.loc[mask].dropna(subset=["tau_40"])
+        inputs_modified = inputs.dropna(subset=["tau_40"])
+
+        inputs_modified["OCR"] = (inputs.Pc / inputs.sigma_vc_eff)
+        inputs_modified["OCR"][inputs_modified["OCR"] < 1] = 1
 
         # run tests
-        (S_test, m_test) = ShansepUtils.get_shansep_parameters(
-            (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
+        (S_test, s_std), (m_test, m_std) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
+            inputs_modified["OCR"],
             inputs_modified.tau_40.to_numpy(),
-            inputs_modified.sigma_v0_eff.to_numpy(),
+            inputs_modified.sigma_vc_eff.to_numpy(),
         )
-        assert S_test - 0.421 < 0.00051
-        assert m_test - 0.789 < 0.00051
+
+        # assert
+        assert pytest.approx(S_test, abs=0.00051) == 0.386
+        assert pytest.approx(m_test, abs=0.00051) == 0.926
+
+        assert pytest.approx(s_std, abs=0.00051) == 0.005
+        assert pytest.approx(m_std, abs=0.00051) == 0.027
 
         # test with a given S parameter
-        (S_output, m_output) = ShansepUtils.get_shansep_parameters(
-            (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
+        (S_output, s_std), (m_output, m_std) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
+            inputs_modified["OCR"],
             inputs_modified.tau_40.to_numpy(),
-            inputs_modified.sigma_v0_eff.to_numpy(),
+            inputs_modified.sigma_vc_eff.to_numpy(),
             S=S_test,
         )
 
         assert S_output == S_test
-        assert m_output - m_test < 0.00051
+        assert abs(m_output - m_test) < 0.0051
 
         # test with a given m parameter
-        (S_output, m_output) = ShansepUtils.get_shansep_parameters(
-            (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
+        (S_output, s_std), (m_output, m_std) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
+            inputs_modified["OCR"],
             inputs_modified.tau_40.to_numpy(),
-            inputs_modified.sigma_v0_eff.to_numpy(),
+            inputs_modified.sigma_vc_eff.to_numpy(),
             m=m_test,
             S=None,
         )
 
-        assert S_output - S_test < 0.00051
+        assert abs(S_output - S_test) < 0.0051
         assert m_test == m_output
 
     @pytest.mark.unittest
@@ -60,14 +67,14 @@ class TestShanshepUtils:
         mask = inputs.TestConditions == "In situ"
         inputs_modified = inputs.loc[mask].dropna(subset=["tau_40"])
         # test with a given S parameter
-        (S_test, m_test) = ShansepUtils.get_shansep_parameters(
+        (S_test, m_test) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
             (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
             inputs_modified.tau_40.to_numpy(),
             inputs_modified.sigma_v0_eff.to_numpy(),
         )
 
         # If a higher m is inputted a smaller log(S) should be produced
-        (S_output, m_output) = ShansepUtils.get_shansep_parameters(
+        (S_output, m_output) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
             (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
             inputs_modified.tau_40.to_numpy(),
             inputs_modified.sigma_v0_eff.to_numpy(),
@@ -77,7 +84,7 @@ class TestShanshepUtils:
         assert np.log(S_output) < np.log(S_test)
 
         # If a lower m is inputted a higher log(S) should be produced
-        (S_output, m_output) = ShansepUtils.get_shansep_parameters(
+        (S_output, m_output) = ShansepUtils.get_shansep_prob_parameters_with_linear_regression(
             (inputs_modified.Pc / inputs_modified.sigma_v0_eff).to_numpy(),
             inputs_modified.tau_40.to_numpy(),
             inputs_modified.sigma_v0_eff.to_numpy(),
