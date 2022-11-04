10648de8da4b29a699675b4b2ff92f6feb283d33
Mark van der Krogt <mark.vanderkrogt@deltares.nl>
2022-11-04 16:23:30 +0100

    corrected the code input definition, added functionality for bounds, removed obsolete code

--- geolib_plus/shm/regression_utils.py
+++ geolib_plus/shm/regression_utils.py
@@ -22,31 +22,28 @@ class RegressionUtils(BaseModel):
     def linear_regression(
         x: Union[float, np.array],
         y: Union[float, np.array],
-        ) -> (dict, dict):
+        bounds=([-np.inf,-np.inf],[np.inf,np.inf]) ,
+        ) -> (dict, dict, tuple):
         """
         """
 
         # initialise covariance matrix
         covariance_matrix = np.zeros((2, 2))
 
-        popt, cov = curve_fit(ShansepUtils.__linear, x, y, method="lm")
+        if np.any((np.array(bounds[0]) > -np.inf) | (np.array(bounds[1]) < np.inf )): #bounded problem use trf
+            method = 'trf'
+        else: #unbounded problem use levenberg maquard
+            method='lm'
+
+        popt, cov = curve_fit(RegressionUtils.__linear, x, y, method=method, bounds=bounds)
         slope, intercept = popt
         # point statistics
         std_slope, std_intercept, covariance = np.sqrt(cov[0,0]), np.sqrt(cov[1, 1]), cov[0,1]
         rho = covariance / (std_slope * std_intercept)
 
         N = len(x)
-        y_fit = ShansepUtils.__linear(x, slope, intercept)
+        y_fit = RegressionUtils.__linear(x, slope, intercept)
         residuals = np.sum((y - y_fit) ** 2)
-
-        def regression_function(x, alpha, quantile):
-            Z = (std_intercept ** 2) \
-                + (x ** 2 * std_slope ** 2) \
-                + 2 * rho * x * std_intercept * std_slope \
-                + (1 - alpha) * residuals / (N - 2)
-
-            tz = t(N - 2).ppf(quantile) * np.sqrt(Z)
-            y = (a * x + b) + tz
 
         fit_params = {'slope':slope,
                       'intercept':intercept,
@@ -54,11 +51,23 @@ class RegressionUtils(BaseModel):
                       'std_intercept':std_intercept,
                       }
 
-        other_params = {'n':n,
+        other_params = {'N':N,
                         'rho':rho,
                         'covariance':covariance,
                         'residuals':residuals
                         }
+
+        def regression_function(x, alpha, quantile):
+            Z = (std_intercept ** 2) \
+                + (x ** 2 * std_slope ** 2) \
+                + 2 * rho * x * std_intercept * std_slope \
+                + (1 - alpha) * residuals / (N - 2)
+
+            from scipy.stats import t
+            tz = t(N - 2).ppf(quantile) * np.sqrt(Z)
+            y = (slope * x + intercept) + tz
+
+            return y
 
         return fit_params, other_params, regression_function
 
