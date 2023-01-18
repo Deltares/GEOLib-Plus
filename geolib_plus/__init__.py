3d23c33e8a47e6ce6261e6e8b7e36a1e253f0556
EleniSmyrniou <eleni.smyrniou@deltares.nl>
2023-01-18 17:05:30 +0100

    Tests corrected

--- geolib_plus/__init__.py
+++ geolib_plus/__init__.py
@@ -1,16 +1,9 @@
 from .cpt_base_model import *
-from .bro_xml_cpt import *
-from .cpt_utils import *
-from .robertson_cpt_interpretation import *
-from .shm import *
+from .bro_xml_cpt import bro_utils, bro_xml_cpt, validate_bro
+from .cpt_utils import cpt_utils, NEN9997, pwp_reader
+from .robertson_cpt_interpretation import robertson_cpt_interpretation
+from .shm import soil, shm_tables, state_utils, shansep_utils, nkt_utils, general_utils, prob_utils
 
-import geolib_connections
-import hardening_soil_model_parameters
-import plot_cpt
-import plot_dynamic_map
-import plot_settings
-import plot_utils
-import relative_density_correlated_parametes
-import soft_soil_creep_parameters
+from . import *
 
 __version__ = "0.2.0"
