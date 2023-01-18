fef4ac48bbfe2063c6b1b05b5c0527a1111a9d36
EleniSmyrniou <eleni.smyrniou@deltares.nl>
2023-01-18 16:37:23 +0100

    Update the init file to a more generic version

--- geolib_plus/__init__.py
+++ geolib_plus/__init__.py
@@ -1,8 +1,8 @@
 from .cpt_base_model import *
-from .bro_xml_cpt import bro_utils, bro_xml_cpt, validate_bro
-from .cpt_utils import cpt_utils, NEN9997, pwp_reader
-from .robertson_cpt_interpretation import robertson_cpt_interpretation
-from .shm import general_utils, nkt_utils, prob_utils, shansep_utils, shm_tables, soil, state_utils
+from .bro_xml_cpt import *
+from .cpt_utils import *
+from .robertson_cpt_interpretation import *
+from .shm import *
 
 import geolib_connections
 import hardening_soil_model_parameters
