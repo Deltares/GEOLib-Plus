09eb257c88da63316a7297a6028000dc8b9bc180
Mark van der Krogt <mark.vanderkrogt@deltares.nl>
2023-02-10 09:08:28 +0100

    cleanup and added expanation

--- geolib_plus/shm/regression_utils.py
+++ geolib_plus/shm/regression_utils.py
@@ -1,51 +1,53 @@
+# import packages
 import warnings
-from typing import Optional, Union
-
 import numpy as np
+
+from typing import Optional, Union
 from pydantic import BaseModel
 from scipy.optimize import curve_fit
-
 from geolib_plus.shm.prob_utils import ProbUtils
 
 
 class RegressionUtils(BaseModel):
+
     class Config:
         arbitrary_types_allowed = True
 
     @staticmethod
     def __linear(x, slope, intercept):
         """
-        Linear funciton as input for optimizing routine
+        Function for linear regression. Use as input within optimizing routine.
         """
         return slope * x + intercept
 
     @staticmethod
     def linear_regression(
-        x: Union[float, np.array],
-        y: Union[float, np.array],
-        bounds=([-np.inf, -np.inf], [np.inf, np.inf]),
+            x: Union[float, np.array],
+            y: Union[float, np.array],
+            bounds=([-np.inf, -np.inf], [np.inf, np.inf]),
     ) -> (dict, dict, tuple):
         """
-        Do a linear regression between x and y. The inputs x and y are arrays.
-        You can specify bounds for the intercept and slope parameter.
-        In case of an unbounded problem the 'trf' method is used in stead of Levenberg Maquard.
+        Method for linear regression between x and y. The inputs x and y are arrays.
+        The user may specify the bounds for the regression coefficients intercept and slope paramete.
+        If the bounds are specified, the 'Trust Region Reflective algorithm' method is used in the optimizing routing,
+        for unbounded problems the 'Levenberg-Marquardt' method is used.
         """
 
         # initialise covariance matrix
         covariance_matrix = np.zeros((2, 2))
 
-        if np.any(
-            (np.array(bounds[0]) > -np.inf) or (np.array(bounds[1]) < np.inf)
-        ):  # bounded problem use trf
+        # For a bounded problem use trf method, for unbounded problem use levenberg maquard
+        if np.any((np.array(bounds[0]) > -np.inf) or (np.array(bounds[1]) < np.inf)):
             method = "trf"
-        else:  # unbounded problem use levenberg maquard
+        else:
             method = "lm"
 
         popt, cov = curve_fit(
             RegressionUtils.__linear, x, y, method=method, bounds=bounds
         )
         slope, intercept = popt
-        # point statistics
+
+        # get the standard deviation and  covariance of the residuals
         std_slope, std_intercept, covariance = (
             np.sqrt(cov[0, 0]),
             np.sqrt(cov[1, 1]),
@@ -71,12 +73,27 @@ class RegressionUtils(BaseModel):
             "residuals": residuals,
         }
 
-        def regression_function(x, alpha, quantile):
+
+        def regression_function(x, alpha=0, quantile=0.05):
+            '''
+            After fitting the regression, the regression function y = slope * x + intercept is returned.
+            A specific upper or lower limit of the regression line can be retrieved by specifying the:
+            - quantile (default 5% lower limit: quantile = 0.05).
+            The function accounts for statistical uncertainty (Student-t distribution) and the uncertainty
+            of the fit (based on the residuals). An additional parameter
+            - alpha can be specified to apply variance reduction, in order to account for spatial averaging. The parameter
+            alpha refers to alpha = 1- Gamma^2. Typical values are:
+                - alpha = 1     : full avaraging, uncertainty in the average
+                - alpha = 0.75  : partly avaraging, regional data where 75% of the variance is assumed to average.
+                - alpha = 0     : no averaging, uncertainty in single point value.
+
+            '''
+
             Z = (
-                (std_intercept**2)
-                + (x**2 * std_slope**2)
-                + 2 * rho * x * std_intercept * std_slope
-                + (1 - alpha) * residuals / (N - 2)
+                    (std_intercept ** 2)
+                    + (x ** 2 * std_slope ** 2)
+                    + 2 * rho * x * std_intercept * std_slope
+                    + (1 - alpha) * residuals / (N - 2)
             )
 
             from scipy.stats import t
