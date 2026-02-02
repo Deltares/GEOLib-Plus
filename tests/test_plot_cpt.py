f41140dbc61efaa7b23e6814ac9b349b918fe314
smyrniou <eleni.smyrniou@deltares.nl>
2026-02-02 12:56:25 +0100

    configure matplotlib to use a non-interactive backend

--- tests/test_plot_cpt.py
+++ tests/test_plot_cpt.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 
+from pathlib import Path
+import matplotlib
 import numpy as np
 import pytest
 
@@ -9,6 +10,7 @@ from geolib_plus.gef_cpt import GefCpt
 from geolib_plus.plot_settings import PlotSettings
 from tests.utils import TestUtils
 
+matplotlib.use('Agg')
 
 class TestPlotCpt:
     @pytest.mark.unittest
